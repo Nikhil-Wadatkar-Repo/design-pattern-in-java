d815727eefdde743dbc03bf1e6c3313bc829ec63
nikhil.wadatkar4693@gmail.com <nikhil.wadatkar4693@gmail.com>
2023-02-12 17:44:24 +0530

    FactoryInterface added

--- src/main/java/com/bs/designPatterns/DP_1_factory/FactoryInterface.java
+++ src/main/java/com/bs/designPatterns/DP_1_factory/FactoryInterface.java
@@ -1,7 +1,7 @@
-package com.bs;
+package com.bs.designPatterns.DP_1_factory;
 
 public class FactoryInterface {
     public static void main(String[] args) {
-
+        System.out.println(123);
     }
 }
